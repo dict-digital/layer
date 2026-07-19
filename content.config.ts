c78c7a67bab5028b8c537f71c0feddfdd2cca5c8
Wing <114337707+wing-0902@users.noreply.github.com>
2026-07-19 19:49:44 +0900

    Refactor content source configuration in content.config.ts
    Docusの実装例を参考にした

--- content.config.ts
+++ content.config.ts
@@ -1,13 +1,22 @@
 import { defineContentConfig, defineCollection } from '@nuxt/content';
 import { z } from 'zod';
 
+import { useNuxt } from '@nuxt/kit'
+import { joinURL } from 'ufo'
+
+const { options } = useNuxt()
+const cwd = joinURL(options.rootDir, 'content')
+
 console.log("CONTENT CONFIG LOADED");
 
 export default defineContentConfig({
   collections: {
     dictionary: defineCollection({
       type: 'page',
-      source: 'dict/**/index.md',
+      source: {
+        cwd,
+        include: 'dict/**/index.md'
+      },
       schema: z.object({
         title: z.string(),
         category: z.string().optional(),
