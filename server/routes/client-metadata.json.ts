e5dfb0ce532bd9b471d7f5254776be93e348b18a
Wing <114337707+wing-0902@users.noreply.github.com>
2026-08-04 04:50:06 +0000

    エラーを修正

--- server/routes/client-metadata.json.ts
+++ server/routes/client-metadata.json.ts
@@ -25,6 +25,7 @@ export default defineEventHandler((event) => {
     grant_types: ['authorization_code', 'refresh_token'],
     response_types: ['code'],
     token_endpoint_auth_method: 'none',
-    application_type: 'web'
+    application_type: 'web',
+    dpop_bound_access_tokens: true
   };
 });
